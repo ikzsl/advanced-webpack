049709447ac1acc8de9dc1673d082de9fe3693de
Джалалудинов Муслим <dzhalaludinov@proitr.ru>
2023-12-10 19:20:49 +0300

    hw2: add config

--- 2/webpack.config.js
+++ 2/webpack.config.js
@@ -3,6 +3,15 @@ const path = require('node:path');
 module.exports = {
 	devtool: false,
 	mode: 'development',
-
-	// ...ваш конфиг
-}
+	entry: {
+		app: { import: './src/app.js', dependOn: 'vendor' },
+		home: { import: './src/pages/home.js', dependOn: 'vendor' },
+		login: { import: './src/pages/login.js' },
+		vendor: ['react', 'react-dom'],
+	},
+	output: {
+		path: path.resolve('build'),
+		filename: '[name].[hash].js',
+		// clean: true,
+	},
+};
